c3374dcd955ca9dbd6c5621e3d6e8aa98e2c8f13
John Cameron <jcameron2@mail.usf.edu>
2019-03-02 11:43:22 -0500

    Create openjdk-11-linux.sh

---
+++ openjdk-11-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz -O openjdk-11.0.2_linux-x64_bin.tar.gz
+echo "Verifying checksum..."
+echo "99be79935354f5c0df1ad293620ea36d13f48ec3ea870c838f20c504c9668b57  openjdk-11.0.2_linux-x64_bin.tar.gz" | sha1sum --check
+echo "Extracting..."
+tar -xzf community_images.tar.gz -C /opt/jdk
+echo "Cleaning..."
+rm openjdk-11.0.2_linux-x64_bin.tar.gz
+echo "Updating paths..."
+update-alternatives --install /usr/bin/java java /opt/jdk/jdk-11.0.2/bin/java 100
+update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk-11.0.2/bin/javac 100
+echo "Installation Complete"
