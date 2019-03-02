129cbc27059446ccdf3e6e5d3591d2f39f56ae82
John Cameron <jcameron2@mail.usf.edu>
2019-03-02 11:46:22 -0500

    Update openjdk-11-linux.sh

--- openjdk-11-linux.sh
+++ openjdk-11-linux.sh
@@ -2,9 +2,9 @@
 
 wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz -O openjdk-11.0.2_linux-x64_bin.tar.gz
 echo "Verifying checksum..."
-echo "99be79935354f5c0df1ad293620ea36d13f48ec3ea870c838f20c504c9668b57  openjdk-11.0.2_linux-x64_bin.tar.gz" | sha1sum --check
+echo "99be79935354f5c0df1ad293620ea36d13f48ec3ea870c838f20c504c9668b57  openjdk-11.0.2_linux-x64_bin.tar.gz" | sha256sum --check
 echo "Extracting..."
-tar -xzf community_images.tar.gz -C /opt/jdk
+tar -xzf openjdk-11.0.2_linux-x64_bin.tar.gz -C /opt/jdk
 echo "Cleaning..."
 rm openjdk-11.0.2_linux-x64_bin.tar.gz
 echo "Updating paths..."
